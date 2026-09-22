48585eacf16f7a01358074e362bf8600783b33e5
Mayank Choubey <mayankchoubey321@gmail.com>
2026-09-22 09:51:58 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        for(int i = 0; i<n;i++){
+            ans[i] = nums[i];
+        }
+        ans.insert(ans.end(),nums.begin(),nums.end());
+        return ans;
+        
+
+    }
+};
